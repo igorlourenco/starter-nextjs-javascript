951d1f5fc7fc17b02f68f74f03499b641968d40b
Igor Lourenço <theigorlourenco@gmail.com>
2020-09-22 18:18:49 -0300

    Adição da biblioteca "framer-motion"

--- components/Main.js
+++ components/Main.js
@@ -1,8 +1,14 @@
-import React from "react";
-import { Text } from "@chakra-ui/core";
+import React, {useState} from "react";
+import {motion} from "framer-motion"
 
 export default function Main() {
     return (
-        <Text> Esse é o template inicial para aplicações Next.js </Text>
+        <motion.div
+            animate={{ rotate: 360 }}
+            transition={{ duration: 2 }}
+        >
+
+            STARTER TEMPLATE
+        </motion.div>
     )
 }
